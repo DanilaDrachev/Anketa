e7192549d76c05ded050efb3146ec45505b42e6c
Drachev Danila <tmdonoy@gmail.com>
2022-10-09 21:57:43 +0300

    second commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Post {
+    FormDate birthday = new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public boolean subscription;
+
+
+}
+
